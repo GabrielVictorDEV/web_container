6d079c237c1c1b6944a7626b06d8480d61e7d1c4
Gabriel Victor <gv2074300@gmail.com>
2023-09-25 13:33:11 -0300

    criando conexão

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 import './style.css'
+import { WebContainer } from '@webcontainer/api';
+import { files } from './files';
 
+
+// Configurações 
+/** @type {import('@webcontainer/api').WebContainer}  */
+let webcontainerInstance;
+
+window.addEventListener('load', async () => {
+  textareaEl.value = files['index.js'].file.contents;
+
+  // Call only once
+  webcontainerInstance = await WebContainer.boot();
+  await webcontainerInstance.mount(files);
+
+  const packageJSON = await webcontainerInstance.fs.readFile('package.json', 'utf-8');
+  console.log(packageJSON);
+});
+
+// Telas da aplicação
 document.querySelector('#app').innerHTML = `
   <div class="container">
     <div class="editor">
