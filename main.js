db02afc6b1bc81d01ea80d8d5b0736a7a2b953b3
Gabriel Victor <gv2074300@gmail.com>
2023-09-25 13:11:50 -0300

    Criando layout simples da aplicação

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = ``;
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <div class="editor">
+      <textarea>I am a textarea</textarea>
+    </div>
+    <div class="preview">
+      <iframe src="loading.html"></iframe>
+    </div>
+  </div>
+`
+
+/** @type {HTMLIFrameElement | null} */
+const iframeEl = document.querySelector('iframe');
+
+/** @type {HTMLTextAreaElement | null} */
+const textareaEl = document.querySelector('textarea');
+
+
